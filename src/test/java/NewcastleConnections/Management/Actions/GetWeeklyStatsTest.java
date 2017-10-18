84738231ab0f410d4e5aa63a981d828658758020
ScottWalkerAU <ScottWalkerAU@gmail.com>
2017-10-18 19:58:15 +1100

    Add Management tests after switching to testing Database

---
+++ src/test/java/NewcastleConnections/Management/Actions/GetWeeklyStatsTest.java
@@ -0,0 +1,17 @@
+package NewcastleConnections.Management.Actions;
+
+import org.apache.struts2.StrutsJUnit4TestCase;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Scott on 18/10/2017.
+ */
+public class GetWeeklyStatsTest extends StrutsJUnit4TestCase<GetWeeklyStats> {
+
+    @Override
+    protected String getConfigPath() {
+        return "struts-test.xml";
+    }
+
+}
---
+++ src/test/java/NewcastleConnections/Management/Actions/SubmitOfferTest.java
@@ -0,0 +1,17 @@
+package NewcastleConnections.Management.Actions;
+
+import org.apache.struts2.StrutsJUnit4TestCase;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Scott on 18/10/2017.
+ */
+public class SubmitOfferTest extends StrutsJUnit4TestCase<SubmitOffer> {
+
+    @Override
+    protected String getConfigPath() {
+        return "struts-test.xml";
+    }
+
+}

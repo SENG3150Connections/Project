1eb7397a124c3901eab2deeca165501d922e84d8
ScottWalkerAU <ScottWalkerAU@gmail.com>
2017-10-25 19:29:16 +1100

    Add skeleton for Action tests

---
+++ src/test/java/NewcastleConnections/Actions/CompleteTest.java
@@ -0,0 +1,17 @@
+package NewcastleConnections.Actions;
+
+import org.apache.struts2.StrutsJUnit4TestCase;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Scott on 25/10/2017.
+ */
+public class CompleteTest extends StrutsJUnit4TestCase<Complete> {
+
+    @Override
+    protected String getConfigPath() {
+        return "struts-test.xml";
+    }
+
+}
---
+++ src/test/java/NewcastleConnections/Actions/ResultsTest.java
@@ -0,0 +1,14 @@
+package NewcastleConnections.Actions;
+
+import org.apache.struts2.StrutsJUnit4TestCase;
+
+import static org.junit.Assert.*;
+
+public class ResultsTest extends StrutsJUnit4TestCase<Results> {
+
+    @Override
+    protected String getConfigPath() {
+        return "struts-test.xml";
+    }
+
+}
